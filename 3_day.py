69b861418497d29591f51660dc2de5ab9a0a6015
Яковлева Светлана <svetlana1905.iakovleva@yandex.ru>
2020-12-23 20:22:14 +0300

    Third day

---
+++ 3_day.py
@@ -0,0 +1,21 @@
+def longest_substring(s,template):    
+    right = -1
+    result = ''
+    while right < len(s) - 1:
+        res = ''
+        right += 1  
+        for i in template:            
+            while i == s[right]:                
+                res += i                
+                if right < len(s)-1:
+                    right += 1 
+                else:
+                    break                
+        if len(result) < len(res):
+            result = res     
+    return(result)  
+
+
+print(longest_substring('abbaaaaaaazacc', 'abca')=='abbaaaaaaa')
+print(longest_substring('aaabbaabhtrabcddddb', 'abab') == 'aaabbaab')
+print(longest_substring('zacabazbc', 'zabc')=='zac')
