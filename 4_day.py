05a06a8abbe42a61e5bf271763f2d7728dcfbec4
Яковлева Светлана <svetlana1905.iakovleva@yandex.ru>
2020-12-25 00:38:57 +0300

    version with matrix

--- 4_day.py
+++ 4_day.py
@@ -4,84 +4,58 @@
    Бонус: поле произвольного размера (5x5 или больше), игра идет до 5 в ряд.'''
  
 
-def result(board_list,n):    
-    # по столбцам проверяем равенство элементов
+# Подсчет количества элементов Х(list_elements[0]) и 0(list_elements[1])
+def check(board,i,j,list_elements):    
+    if board[i][j] == 'X':
+        list_elements[0] +=  1
+        list_elements[1] = 0    
+        return list_elements
+    elif board[i][j] == '0':
+        list_elements[1] += 1
+        list_elements[0] = 0
+        return list_elements
+    return [0, 0]
+
+
+# Проверка на выигрыш
+def check_win(list_elements):
+    if list_elements[0] >= 5:
+        return 'X wins'
+    if list_elements[1] >= 5:
+        return '0 wins'
+    return None    
+
+
+# Анализ матрицы на выигрыш
+def result(board,n): 
+
+    # по столбцам и строкам проверяем равенство элементов
     for j in range(n):
-        sum_X = 0
-        sum_O = 0        
-        for i in range(j,n*n,n):
-            if board_list[i] == 'X':
-                sum_X += 1
-                sum_O = 0
-                if sum_X >= 5:
-                    return 'X wins'
-            elif board_list[i] == '0':
-                sum_O += 1
-                sum_X = 0
-                if sum_O >= 5:
-                    return '0 wins'
-            else:
-                break
-
-    # построчно проверяем равенство элементов       
-    for j in range(0,n*n,n):
-        sum_X = 0
-        sum_O = 0
-        
-        for i in range(j,n+j):
-            if  board_list[i] == 'X':
-                sum_X += 1
-                sum_O = 0
-                if sum_X >= 5:
-                    return 'X wins'
-                    
-            elif board_list[i] == '0':
-                sum_O += 1
-                sum_X = 0 
-                if sum_O >= 5:
-                    return '0 wins'
-                   
-            else:
-                break
-
+        list_elements = [0, 0]   
+        list_elements_1 = [0, 0]      
+        for i in range(n):      
+            list_elements = check(board,i,j,list_elements)
+            list_elements_1 = check(board,j,i,list_elements_1)
+            for i in list_elements, list_elements_1:
+                res=check_win(i)
+                if res:
+                    return res 
+   
     # диагональ правая
-    index = 0
-    while index < n * n:
-        sum_X = 0
-        sum_O = 0        
-        if board_list[index] == 'X':
-            sum_X += 1
-            sum_O = 0
-            if sum_X >= 5:
-                    return 'X wins'
-        if board_list[index] == '0':
-            sum_O += 1
-            sum_X = 0
-            if sum_O >= 5:
-                return '0 wins'    
-        index = index + n + 1    
+    list_elements = [0, 0]   
+    for i in range(n):
+        list_elements = check(board,i,i,list_elements)
+        res = check_win(list_elements)
+        if res:
+                return res
 
-    # диагональ левая    
-    index = n - 1
-    while index < n * (n-1):
-        sum_X = 0
-        sum_O = 0        
-        if board_list[index] == 'X':
-            sum_X += 1
-            sum_O = 0
-            if sum_X >= 5:
-                    return 'X wins'
-        if board_list[index] == '0':
-            sum_O += 1
-            sum_X = 0
-            if sum_O >= 5:
-                return '0 wins'    
-        index = index + n - 1
-
-    # ничья
-    if n % 2 == 0 and board_list.count('X') == n * n / 2 or n %2 != 0 and board_list.count('X') == n * n / 2 + 1 :   
-        return 'Draw'
-
+    # диагональ левая 
+    list_elements = [0, 0]   
+    for j in range(n-1,0,-1):
+        list_elements = check(board,n-j-1,j,list_elements)
+        res = check_win(list_elements)
+        if res:
+                return res   
     return None             
 
 
@@ -89,9 +63,11 @@ def who_wins(board, n):
     board_list = [j for i in board for j in i]
     X_count = board_list.count('X')
     O_count = board_list.count('0')
-    res = result(board_list, n)    
+    res = result(board, n)    
     if res is None:
-        if O_count < X_count:
+        if n % 2 == 0 and X_count == n * n / 2 or n %2 != 0 and X_count == n * n / 2 + 1 :   
+            res = 'Draw'
+        elif O_count < X_count:
             res = 'Playing: 0'
         else: 
             res = 'Playing: X' 
@@ -112,4 +88,7 @@ print((who_wins(board,6)) == 'X wins')
 
 board = [ ['0', 'X', '0' ,'0', 'X', '0'], ['X', 'X', 'X', '0', 'X', '0'], ['X', '0', '0','X', '0', 'X'],['0', 'X', '0','0', 'X', '0'], ['0', 'X', 'X','0', 'X', '0'], ['X', 'X', '0', 'X', '0', 'X'] ]
 print((who_wins(board,6)) == 'Draw')
+
+board = [ ['X', '0', '0' ,'0', 'X', '0'], ['X', 'X', 'X', '0', 'X', '0'], ['0', '0', 'X','X', '0', 'X'],['0', 'X', '0','X', '0', '0'], ['0', 'X', 'X','0', 'X', '0'], ['X', 'X', '0', 'X', '0', 'X'] ]
+print((who_wins(board,6)) == 'X wins')
 
